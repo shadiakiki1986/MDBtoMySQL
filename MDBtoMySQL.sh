38175f8c527be4c7cce91cec8253a396c55dbf2a
Vagelis Prokopiou <drz4007@gmail.com>
2016-10-01 17:13:21 +0300

    Fixing checks

--- MDBtoMySQL.sh
+++ MDBtoMySQL.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
 # Table 2-1. MDB Utilities
-
 # Name	Description
 # mdb-tables	list tables in the specified file
 # mdb-schema	generate schema DDL for the specified file
 # mdb-export	generate CSV style output for a table
-# mdb-ver	display the version of the specified file
-# mdb-header	support for using MDB data in C
-# mdb-parsecsv	support for using MDB data in C
-# mdb-sql	command line SQL query tool
 
 echo "-- -----------------------------------------------------------------------";
 echo "-- MDBtoSQL";
@@ -25,34 +20,18 @@ echo "-- or Debian-based system.";
 echo "-- -----------------------------------------------------------------------";
 
 
+# Check if mdbtools are installed.
+
+
 # Get all the info you need.
-sleep 1;
+# sleep 1;
 echo "";
 echo "Please, provide the name of the MySQL user.";
 read user;
 
-if [[ "$user" =~ ([[:punct:]]+ || [[:space:]]+) ]]; then
-	echo "No special characters or spaces are allowed in the username.";
-	echo "Exiting.";
-	exit 1;
-fi
-
 echo "";
 echo "Please, provide the password of the MySQL user.";
 read password;
-
-
-if [[ "$password" =~ [[:space:]]+ ]]; then
-	echo "No spaces are allowed in the password.";
-	echo "Exiting.";
-	exit 1;
-fi
-
-echo "";
-echo "Please, provide the name of the database that will be created.";
-echo "Do not use spaces or any special characters:";
-read db_to_create;
-echo "$db_to_create";
 
 echo "";
 echo "Please, provide the name of the mdb file.";
@@ -60,13 +39,58 @@ echo "Make sure to provide the full path,
 if the file is not in the current directory.";
 read db_to_read;
 
-# db_Movies.mdb
+# Check if the file exists.
+if [[ ! -f "$db_to_read" ]]; then
+	echo "The file was not found.";
+	echo "Check the name and the path and try again.";
+	echo "Aborting.";
+	exit 1;
+fi
 
+# Check the name provided.
+if [[ "$db_to_read" =~ [\],!,\\,@,#,$,%,^,\&,\*,\(,\),\?,\<,\>,{,},\[]+ ]] || [[ "$db_to_read" =~ [[:space:]]+ ]]; then
+	echo "It is preferable to not include any special characters or spaces in the name.";
+	echo "Please, rename the file and try again.";
+	echo "Aborting.";
+	exit 1;
+fi
+
+echo "";
+echo "Please, provide the name of the database that will be created.";
+echo "Do not use spaces or any special characters:";
+read db_to_create;
+# echo "$db_to_create";
+
+if [[ "$db_to_create" =~ [\],!,\\,@,#,$,%,^,\&,\*,\(,\),\?,\<,\>,{,},\[]+ ]] || [[ "$db_to_create" =~ [[:space:]]+ ]]; then
+	echo "No special characters or spaces are allowed in the database name.";
+	echo "Aborting.";
+	exit 1;
+fi
+
+# Read the tables from the file.
 tables=$(mdb-tables $db_to_read);
 #tables=$(mdb-tables $db_to_read | tr " "  "\n");
 #for table in $tables; do echo "$table"; done;
 
 # Create the database.
+echo "";
+echo "!!! Attention !!!";
+echo "If there already is a database with the same name, it is about to be deleted!!!";
+echo "Press \"Y\" to confirm and continue.";
+echo "Press \"N\" to abort the operation.";
+read proceed;
+
+if [[ $proceed == "y" ]] || [[ $proceed == "Y" ]]; then
+    echo "Continuing";
+elif [[ $proceed == "n" ]] || [[ $proceed == "N" ]]; then
+    echo "Aborthing";
+    exit 1;
+else
+	echo "The expected input was invalid";
+	echo "Aborting";
+	exit 1;
+fi
+
 mysql -u$user -p$password -e "DROP DATABASE IF EXISTS $db_to_create";
 mysql -u$user -p$password -e "CREATE DATABASE $db_to_create DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;";
 echo "";
@@ -89,7 +113,7 @@ echo "<------------------------------------------------------------------------>
 # Remove stuff I dont want.
 query=$(
 mdb-schema db.mdb  \
-| sed -r 's/(\[[a-zA-Z0-9Α-Ωα-ω]+)(\ )/\1_/g' \
+| sed -r 's/(\[[a-zA-Z0-9]+)(\ )/\1_/g' \
 | sed "s/type.*/VARCHAR (255),/g" \
 | sed "s/\]//g" \
 | sed "s/\[//g" \
@@ -107,8 +131,8 @@ mdb-schema db.mdb  \
 );
 
 
-mysql -uroot -proot -e "USE $db_to_create $query";
+mysql -u"$user" -p"$password" -e "USE $db_to_create $query";
 echo "";
 echo "<------------------------------------------------------------------------>"
-echo "           The tables of \"$db_to_create\" were successfully created."
+echo "           The tables of the \"$db_to_create\" database were successfully created."
 echo "<------------------------------------------------------------------------>"
